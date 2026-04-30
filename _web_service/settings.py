278665c228732ea266ef2f6ad148d176253cc636
Alex <sasha.kirillov.minsk@gmail.com>
2026-04-30 15:15:35 +0300

    add: cache

--- _web_service/settings.py
+++ _web_service/settings.py
@@ -213,6 +213,15 @@ else:
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
